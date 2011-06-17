4f8dba937aaa26fbc76deca0d0816048e59ab772
Marius Grigaitis <admin@mar.lt>
2011-06-18 00:06:47 +0300

    Added comments

--- viktorina/spiders/viktorina_spider.py
+++ viktorina/spiders/viktorina_spider.py
@@ -13,24 +13,29 @@ class ViktorinaSpider(CrawlSpider):
     ]
 
     rules = (
+        # go through all links containing ending with mokslas/
         Rule(SgmlLinkExtractor(allow = ("mokslas/$",)), callback='parse_item', follow = True),
     )
 
     def parse_item(self, response):
         body = response.body
+
+        # find all messages by <Viktorina>
         lines = re.findall("&lt;Viktorina&gt; ([^<]*)<br>", body)
-#        self.log("Found: %s" % lines)
 
         question = None
         items = []
 
         for line in lines:
+            # check for question
             match = re.match("^\x0300,14 \x02[0-9]+\.\x02\x0300,02 (.*) $", line)
             if match is not None:
                 question = match.group(1)
+            # check for confirmed answer
             match = re.match(".*atsakymas( buvo)?: \x0304\x1f([^\x1f]+)\x1f\x0314", line)
             if match is not None:
                 answer = match.group(2)
+                # question and answer found, add it to items
                 if (question is not None and answer is not None):
                     items.append(ViktorinaItem(question = question, answer = answer))
 
--- xchat/autoanswer.py
+++ xchat/autoanswer.py
@@ -1,4 +1,4 @@
-__module_name__ = "Marius quiz script" 
+__module_name__ = "Quiz auto answer script" 
 __module_version__ = "1.0.0" 
 __module_description__ = "Auto answer" 
 
@@ -13,21 +13,30 @@ def on_message(data, something, somethingb):
     message = data[1]
     
     if (nick == "Anna"):
+        # check for question
         match = re.match(".*[0-9]+th Quiz Question:\x02 (.*)$", message)
         if (match):
+            # remove some non printale chars and uppercase question
             question = match.group(1).replace("\xc2\xa0", " ").upper()
             best_ratio = 0.0
             best_answer = None
             answers = csv.reader(open('../db/questions.csv', 'r'))
+
+            # go through all questions in database
             for row in answers:
                 test_answer, test_question = row
+
+                # calculate approximate matching ratio
                 ratio = Levenshtein.ratio(test_question, question)
+
+                # check if it's best possible answer
                 if ratio > best_ratio:
                     best_ratio = ratio
                     best_answer = row
 
             print "Answer: %s (%s) q: %s" % (best_answer[0], best_ratio, best_answer[1])
-#            if best_ratio > 0.80:
-#                xchat.get_context().command("say %s" % (best_answer[0].lower()))
+            # auto answering
+            #if best_ratio > 0.80:
+            #    xchat.get_context().command("say %s" % (best_answer[0].lower()))
                  
 xchat.hook_print('Channel Message', on_message)
